fb4a2fb83ba5301413442dee2353f9f908397610
Yejin <lilyhong511@gmail.com>
2025-04-09 11:33:29 +0900

    start2

--- main.cpp
+++ main.cpp
@@ -4,27 +4,32 @@
 #include "arphdr.h"
 
 #pragma pack(push, 1)
-struct EthArpPacket final {
+struct EthArpPacket final
+{
 	EthHdr eth_;
 	ArpHdr arp_;
 };
 #pragma pack(pop)
 
-void usage() {
-	printf("syntax: send-arp-test <interface>\n");
-	printf("sample: send-arp-test wlan0\n");
+void usage()
+{
+	printf("syntax : send-arp <interface> <sender ip> <target ip> [<sender ip 2> <target ip 2> ...]\n");
+	printf("sample : send-arp wlan0 192.168.10.2 192.168.10.1\n");
 }
 
-int main(int argc, char* argv[]) {
-	if (argc != 2) {
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
 		usage();
 		return EXIT_FAILURE;
 	}
 
-	char* dev = argv[1];
+	char *dev = argv[1];
 	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* pcap = pcap_open_live(dev, 0, 0, 0, errbuf);
-	if (pcap == nullptr) {
+	pcap_t *pcap = pcap_open_live(dev, 0, 0, 0, errbuf);
+	if (pcap == nullptr)
+	{
 		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
 		return EXIT_FAILURE;
 	}
@@ -45,8 +50,9 @@ int main(int argc, char* argv[]) {
 	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
 	packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
 
-	int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	if (res != 0) {
+	int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char *>(&packet), sizeof(EthArpPacket));
+	if (res != 0)
+	{
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
 	}
 
